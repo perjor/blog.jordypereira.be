d3ab23033150f3c42b258a93e1c8a86f26c9306b
Jordy Pereira <jordy.pereira@prorenata.se>
2019-02-16 18:06:02 +0100

    Center overview links

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,15 @@
     <div class="overview">
       <div v-for="post in publishedPosts" :key="post.node.id" class="flex flex-col">
       <g-link :to="post.node.path" class="my-5 text-lg sm:text-xl md:text-2xl no-underline text-black hover:text-brand transition-1">
-        <span class="font-italic text-sm text-grey-dark">{{ post.node.date.split('T')[0] }}</span> {{ post.node.title }} &#8594;
+        <div class="flex justify-between">
+          <div class="flex items-center">
+            <span class="font-italic text-sm text-grey-dark mr-2 flex-no-shrink">{{ post.node.date.split('T')[0] }}</span> 
+            <span>{{ post.node.title }}</span>
+          </div>
+          <div class="self-center text-3xl">
+           &#8594;
+          </div>
+        </div>
       </g-link>
     </div>
     </div>
