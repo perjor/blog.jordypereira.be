d3d2788ee66f7190a8c021984e4d74c31fabf242
Jordy Pereira <jordy.pereira@prorenata.se>
2019-02-17 14:40:42 +0100

    Added filter at the top

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,9 @@
 <template>
   <Layout>
     <div class="overview">
+      <div class="filters flex justify-center text-xs">
+        <span>Filter on: &nbsp;</span><span :class="filterClass(filter)" v-for="(filter, i) in filters" :key="i" @click="toggleFilter(filter)">{{ filter }} &nbsp;</span>
+      </div>
       <div v-for="post in publishedPosts" :key="post.node.id" class="flex flex-col">
       <g-link :to="post.node.path" class="my-5 text-lg sm:text-xl md:text-2xl no-underline text-black hover:text-brand transition-1">
         <div class="flex justify-between">
@@ -62,6 +65,31 @@ export default {
         }
         return false;
       })
+    },
+    filters() {
+      const filters = [];
+      filters.push('travels');
+      filters.push('internship');
+
+      return filters;
+    },
+
+  },
+
+  methods: {
+    toggleFilter(filter) {
+      this.currentFilter = this.currentFilter === filter ? '' : filter;
+    },
+
+    filterClass(filter) {
+      const classes = ['text-grey-dark', 'hover:text-brand', 'cursor-pointer', 'transition-1'];
+
+      if (this.currentFilter === filter) {
+        classes.shift();
+        classes.push('text-brand');
+      }
+
+      return classes;
     },
   },
 }
