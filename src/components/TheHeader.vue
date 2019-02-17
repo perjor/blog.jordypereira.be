c5c2afb5eeafb83026c5437300e448630e69eb93
Jordy Pereira <jordy.pereira@prorenata.se>
2019-02-17 12:09:58 +0100

    Thin font above mobile

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
 <header class="header py-3 hover:shadow transition-1" :class="{shadow: $route.path === '/'}">
   <div class="pl-5">
-    <g-link :to="{ name: 'home' }" class="text-brand font-thin no-underline hover:text-black transition-1">{{ $static.metaData.siteName }}</g-link>
+    <g-link :to="{ name: 'home' }" class="text-brand sm:font-thin no-underline hover:text-black transition-1">{{ $static.metaData.siteName }}</g-link>
   </div>
   <nav class="mx-5 text-right">
     <span class="text-brand">Currently doing an internship in Stockholm!</span>
